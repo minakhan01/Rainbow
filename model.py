6aaa259705687cee3ce5623acbce6d42b561691e
minakhan01 <minakhan01@gmail.com>
2021-04-13 12:14:29 -0400

    1 convolution

--- model.py
+++ model.py
@@ -58,7 +58,7 @@ class DQN(nn.Module):
                                  nn.Conv2d(64, 64, 3, stride=1, padding=0), nn.ReLU())
       self.conv_output_size = 3136
     elif args.architecture == 'data-efficient':
-      self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 5, stride=5, padding=0), nn.ReLU(),
+      self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 1, stride=5, padding=0), nn.ReLU(),
                                  nn.Conv2d(32, 64, 5, stride=5, padding=0), nn.ReLU())
       self.conv_output_size = 576
     self.fc_h_v = NoisyLinear(self.conv_output_size, args.hidden_size, std_init=args.noisy_std)
